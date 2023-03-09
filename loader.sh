f62110dec429e3b0e16055b8be15bd24c234e4cf
dxvx <47729249+dxvx@users.noreply.github.com>
2023-03-09 14:48:41 +0800

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -6,7 +6,7 @@ VDOMAIN=$4
 
 # install basic packages
 sudo apt update
-sudo apt install -y dos2unix wget zip unzip python3-pip git net-tools curl snap
+sudo apt install -y git
 
 sudo git clone  https://$USERNAME:$PASSWORD@github.com/$USERNAME/v4.git /v2ok
 sudo bash /v2ok/install.sh $VZONE $VDOMAIN
