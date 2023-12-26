d636e4d56e0668099c18dc598562d5ad2f68cc37
dxvx <47729249+dxvx@users.noreply.github.com>
2023-12-26 14:58:16 +0800

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -4,6 +4,13 @@ PASSWORD=$2
 VZONE=$3
 VDOMAIN=$4
 
+if [ ! -d "/v2ok" ]; then
+  echo "v2ok nooooooot exist"
+else
+  echo "init complete, exit"
+  exit
+fi
+
 # install basic packages
 sudo apt update
 sudo apt install -y git
